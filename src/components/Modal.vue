ce22a50da863c142687e4d9f27c48a6742b3a94f
Gia Bao <giabao.231999@gmail.com>
2022-06-29 11:04:37 +0700

    finish

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -11,17 +11,23 @@
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import { mapMutations, mapState } from "vuex";
 export default {
   methods: {
-    ...mapMutations(["TOGGLE_MODAL", "TOGGLE_INVOICE"]),
+    ...mapMutations(["TOGGLE_MODAL", "TOGGLE_INVOICE","TOGGLE_EDIT_INVOICE"]),
     closeModal() {
       this.TOGGLE_MODAL();
     },
     closeInvoice() {
       this.TOGGLE_INVOICE();
       this.TOGGLE_MODAL();
+      if(this.editInvoice){
+        this.TOGGLE_EDIT_INVOICE();
+      }
     },
+  },
+  computed: {
+   ...mapState(['editInvoice'])
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,5 @@
-import {
-  createStore
-} from 'vuex'
-import db from '../firebase/firebaseInit'
+import { createStore } from "vuex";
+import db from "../firebase/firebaseInit";
 
 export default createStore({
   state: {
@@ -10,13 +8,14 @@ export default createStore({
     modalActive: null,
     invoicesLoaded: null,
     currentInvoiceArray: null,
+    editInvoice: null,
   },
   mutations: {
     TOGGLE_INVOICE(state) {
-      state.invoiceModal = !state.invoiceModal
+      state.invoiceModal = !state.invoiceModal;
     },
     TOGGLE_MODAL(state) {
-      state.modalActive = !state.modalActive
+      state.modalActive = !state.modalActive;
     },
     SET_INVOICE_DATA(state, payload) {
       state.invoiceData.push(payload);
@@ -24,21 +23,41 @@ export default createStore({
     INVOICES_LOADED(state) {
       state.invoicesLoaded = true;
     },
-    SET_CURRENT_INVOICE(state,payload){
-      state.currentInvoiceArray = state.invoiceData.filter(invoice =>{
+    SET_CURRENT_INVOICE(state, payload) {
+      state.currentInvoiceArray = state.invoiceData.filter((invoice) => {
         return invoice.invoiceId === payload;
-      })
-    }
+      });
+    },
+    TOGGLE_EDIT_INVOICE(state) {
+      state.editInvoice = !state.editInvoice;
+    },
+    DELETE_INVOICE(state, payload) {
+      state.invoiceData = state.invoiceData.filter((invoice) => invoice.docId !== payload);
+    },
+    UPDATE_STATUS_TO_PAID(state, payload) {
+      state.invoiceData.forEach((invoice) => {
+        if (invoice.docId === payload) {
+          invoice.invoicePaid = true;
+          invoice.invoicePending = false;
+        }
+      });
+    },
+    UPDATE_STATUS_TO_PENDING(state, payload) {
+      state.invoiceData.forEach((invoice) => {
+        if (invoice.docId === payload) {
+          invoice.invoicePaid = false;
+          invoice.invoicePending = true;
+          invoice.invoiceDraft = false;
+        }
+      });
+    },
   },
   actions: {
-    async GET_INVOICES({
-      commit,
-      state
-    }) {
+    async GET_INVOICES({ commit, state }) {
       const getData = db.collection("invoices");
       const results = await getData.get();
-      results.forEach(doc => {
-        if (!state.invoiceData.some(invoice => invoice.docId === doc.id)) {
+      results.forEach((doc) => {
+        if (!state.invoiceData.some((invoice) => invoice.docId === doc.id)) {
           const data = {
             docId: doc.id,
             invoiceId: doc.data().invoiceId,
@@ -52,25 +71,52 @@ export default createStore({
             clientCity: doc.data().clientCity,
             clientZipCode: doc.data().clientZipCode,
             clientCountry: doc.data().clientCountry,
-            invoiceDate: doc.data().invoiceDate,
             invoiceDateUnix: doc.data().invoiceDateUnix,
+            invoiceDate: doc.data().invoiceDate,
             paymentTerms: doc.data().paymentTerms,
-            paymentDueDate: doc.data().paymentDueDate,
             paymentDueDateUnix: doc.data().paymentDueDateUnix,
+            paymentDueDate: doc.data().paymentDueDate,
             productDescription: doc.data().productDescription,
             invoiceItemList: doc.data().invoiceItemList,
             invoiceTotal: doc.data().invoiceTotal,
             invoicePending: doc.data().invoicePending,
             invoiceDraft: doc.data().invoiceDraft,
             invoicePaid: doc.data().invoicePaid,
-          }
+          };
           commit("SET_INVOICE_DATA", data);
         }
       });
-      commit('INVOICES_LOADED')
-    }
+      commit("INVOICES_LOADED");
+    },
+    async UPDATE_INVOICE({ commit, dispatch }, { docId, routeId }) {
+      commit("DELETE_INVOICE", docId);
+      await dispatch("GET_INVOICES");
+      commit("TOGGLE_INVOICE");
+      commit("TOGGLE_EDIT_INVOICE");
+      commit("SET_CURRENT_INVOICE", routeId);
+    },
+    async DELETE_INVOICE({ commit }, docId) {
+      const getInvoice = db.collection("invoices").doc(docId);
+      await getInvoice.delete();
+      commit("DELETE_INVOICE", docId);
+    },
+    async UPDATE_STATUS_TO_PAID({ commit }, docId) {
+      const getInvoice = db.collection("invoices").doc(docId);
+      await getInvoice.update({
+        invoicePaid: true,
+        invoicePending: false,
+      });
+      commit("UPDATE_STATUS_TO_PAID", docId);
+    },
+    async UPDATE_STATUS_TO_PENDING({ commit }, docId) {
+      const getInvoice = db.collection("invoices").doc(docId);
+      await getInvoice.update({
+        invoicePaid: false,
+        invoicePending: true,
+        invoiceDraft: false,
+      });
+      commit("UPDATE_STATUS_TO_PENDING", docId);
+    },
   },
-  modules: {
-
-  }
-})
+  modules: {},
+});
